3b778a49aec2e2bd0688f5f7c93e9c472eef8857
Larissa Nascimento <50881391+lnascc@users.noreply.github.com>
2021-10-22 17:15:51 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+const linksSocialMedia = {
+  github: 'lnascc',
+  youtube: 'channel/UCvW_bugChdAxj_3zDs6qnwg',
+  facebook: 'rihanna',
+  instagram: 'larihnascimento',
+  twitter: 'lnascc'
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+    li.children[0].href = `https://${social}.com/${linksSocialMedia[social]}`
+  }
+}
+
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${linksSocialMedia.github}`
+
+  fetch(url)
+  .then(response => response.json())
+  .then(data => {
+    userName.textContent = data.name
+    userBio.textContent = data.bio
+    userLink.href = data.html_url
+    userImage.src = data.avatar_url
+    userLogin.href = data.login
+
+  })
+  
+}
+
+getGitHubProfileInfos()
